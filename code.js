0d8d9f9d0d58757f0d4601e7413cfd76daf879b0
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-09-03 12:12:45 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,22 @@
 function divideAndConquerSum(a) {
-    return a;
+    var sum = 0;
+    if (a.length == 0) {
+        return 0;
+    }
+    sum = summify(a, Math.floor(a.length / 3), 2 * Math.floor(a.length / 3), sum);
+
+    return sum;
+}
+
+function summify(a, cut1, cut2, sum) {
+    if (a.length == 2) {
+        return a[0] + a[1];
+    } else if (a.length == 1) {
+        return a[0];
+    } else {
+        cut1 = Math.floor(a.length / 3);
+        cut2 = 2 * Math.floor(a.length / 3);
+        sum = summify(a.slice(0, cut1), cut1, cut2, sum) + summify(a.slice(cut1, cut2), cut1, cut2, sum) + summify(a.slice(cut2, a.length), cut1, cut2, sum);
+        return sum;
+    }
 }
